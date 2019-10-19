92a64b24b81fba29fed342278c7c91ecf76d775d
Saul Blanco Tejero <alu15067@iesgalileo.es>
2019-10-19 20:09:29 +0200

    Now colorful pins can be added

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -9,13 +9,28 @@ function injectData(element, objData) {
     });
 }
 
+// Creates and return a new color selector
 function getColorSelector() {
     let colors = [
         "red",
-        "pink",
+        "blue",
+        "green",
+        "purple",
         "orange",
         "yellow",
     ];
+
+    let selectorElement = document.createElement("select");
+
+    colors.forEach(color => {
+        let colorOption = document.createElement("option");
+        colorOption.setAttribute("value", color);
+        colorOption.innerText = color;
+
+        selectorElement.appendChild(colorOption);
+    });
+
+    return selectorElement;
 }
 
 searchDataButton.addEventListener("click", function () {
@@ -31,17 +46,43 @@ addDataButton.addEventListener("click", function () {
 
     inputCoordName.setAttribute("placeholder", "Name");
     inputCoordName.setAttribute("id", "coordName");
+    pickButton.setAttribute("id", "button-pick");
     pickButton.innerText = "Pick";
     pickButton.addEventListener("click", () => {
-        createPoint("orange");
-        let container = this.parentElement;
-        //let co
-        //container.replaceChild()
+        createPoint(colorSelector.value);
+        let container = pickButton.parentElement;
     });
 
     controls.push(document.createElement("br"));
     controls.push(inputCoordName);
+    controls.push(colorSelector);
     controls.push(pickButton);
 
     this.parentElement.append(...controls);
 });
+
+
+// Creates a paragraph with coords info and removes controls
+export default function generateCoordParagraph() {
+    let container = document.getElementById("add-data").parentElement;
+    let pickButton = document.getElementById("button-pick");
+    let inputCoordName = document.getElementById("coordName");
+    let colorSelector = getColorSelector();
+
+    let coordNameParagraph = document.createElement("span");
+    let delButton = document.createElement("button");
+    delButton.innerText = "x";
+
+    delButton.addEventListener("click", () => {
+        container.removeChild(coordNameParagraph.previousElementSibling);
+        container.removeChild(coordNameParagraph);
+        container.removeChild(delButton);
+    })
+
+    coordNameParagraph.setAttribute("class", colorSelector.value);
+    coordNameParagraph.innerText = inputCoordName.value;
+    
+    container.removeChild(pickButton.previousSibling);
+    container.replaceChild(coordNameParagraph, pickButton.previousSibling);
+    container.replaceChild(delButton, pickButton);
+}
