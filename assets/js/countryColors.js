e9a249410ec489e51984523d8f456f57be6959cd
Carlos Del Cura Pascual <delcuracarlos@gmail.com>
2019-10-19 21:25:40 +0200

    Countries colored with the data

--- assets/js/countryColors.js
+++ assets/js/countryColors.js
@@ -1,14 +1,19 @@
-import { birthRate } from "./csv.js";
+import {
+    birthRate
+} from "./csv.js";
 
+// variable canvas
 var wwd = new WorldWind.WorldWindow("canvasOne");
 
 wwd.addLayer(new WorldWind.BMNGOneImageLayer());
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.AtmosphereLayer());
 
+//Variables colores mapa
 
 //ShapeFiles
 var shapeConfigurationCallback = function (attributes, record) {
+
     var configuration = {};
     configuration.name = attributes.values.name || attributes.values.Name || attributes.values.NAME;
 
@@ -25,26 +30,40 @@ var shapeConfigurationCallback = function (attributes, record) {
         configuration.attributes = new WorldWind.ShapeAttributes(null);
 
         let data = birthRate.split("\n");
+        let currentRateColors;
         let currentRate;
-
+        let maxNumber = 0;
         data.forEach(element => {
-           if(element.split(",")[0] === record.attributes.values.name)
-           {
-               currentRate = parseInt(element.split(",")[1]);
-               console.log(currentRate + " " + map_range(currentRate, 0, 50, 0, 255));
-           } 
+            let currentNumber = parseInt(element.split(",")[1]);
+            if (currentNumber > maxNumber) {
+                maxNumber =  currentNumber;
+            }
         });
+        console.log("max:" + maxNumber);
+        console.log("name:" + record.attributes.values.name);
+        data.forEach(element => {
+            if (element.split(",")[0] === record.attributes.values.name) {
+                currentRate = map_range(parseInt(element.split(",")[1]), 0, maxNumber, 0, 60);
+            }
+        });
+        
+        if (currentRate === undefined) {
+            return configuration;
+        }
 
-
-
+        currentRateColors = color_Map(currentRate);
+        console.log(currentRate);
+        
+        //Color between  0 and 1
         configuration.attributes.interiorColor = new WorldWind.Color(
-            map_range(currentRate, 0, 50, 0, 255),
-            0,
-            0,
+            currentRateColors[0],
+            currentRateColors[1],
+            currentRateColors[2],
             1.0
         );
+        
 
-        // Paint the outline in a darker variant of the interior color.
+        // Paint the outline in a darker variant of the interior color. Fija
         configuration.attributes.outlineColor = new WorldWind.Color(
             0,
             0,
@@ -64,8 +83,31 @@ let worldShapefile = new WorldWind.Shapefile(shapefileLibrary + "/ne_110m_admin_
 worldShapefile.load(null, shapeConfigurationCallback, worldLayer);
 wwd.addLayer(worldLayer);
 
-function map_range(value, low1, high1, low2, high2) 
-{
+
+function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
 
+
+function color_Map(number) {
+    let purple = [0.19, 0.00, 0.40];
+    let darkBlue = [0.00, 0.00, 0.80];
+    let blue = [0.00, 0.40, 1.00];
+    let green = [0.00, 0.80, 0.00];
+    let yellow = [1.00, 0.80, 0.19];
+    let orange = [1.00, 0.40, 0.00];
+
+    if (number > 0 && number <= 10) {
+        return purple;
+    } else if (number > 10 && number <= 20) {
+        return darkBlue;
+    } else if (number > 20 && number <= 30) {
+        return blue;
+    } else if (number > 30 && number <= 40) {
+        return green;
+    } else if (number > 40 && number <= 50) {
+        return yellow;
+    } else if (number > 50 && number <= 60) {
+        return orange;
+    }
+}
