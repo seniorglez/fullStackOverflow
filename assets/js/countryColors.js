f5e26bc8021401b338a4f0b2661348e548a6faaf
Carlos Del Cura Pascual <delcuracarlos@gmail.com>
2019-10-20 10:16:51 +0200

    Clean the code

--- assets/js/countryColors.js
+++ assets/js/countryColors.js
@@ -9,16 +9,15 @@ wwd.addLayer(new WorldWind.BMNGOneImageLayer());
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.AtmosphereLayer());
 
-//Variables colores mapa
 
+/* Functions */
 //ShapeFiles
 var shapeConfigurationCallback = function (attributes, record) {
 
     var configuration = {};
     configuration.name = attributes.values.name || attributes.values.Name || attributes.values.NAME;
 
-    if (record.isPolygonType()) 
-    { 
+    if (record.isPolygonType()) {
         configuration.attributes = new WorldWind.ShapeAttributes(null);
 
         let data = birthRate.split("\n");
@@ -29,7 +28,7 @@ var shapeConfigurationCallback = function (attributes, record) {
         data.forEach(element => {
             let currentNumber = parseInt(element.split(",")[1]);
             if (currentNumber > maxNumber) {
-                maxNumber =  currentNumber;
+                maxNumber = currentNumber;
             }
         });
 
@@ -38,13 +37,10 @@ var shapeConfigurationCallback = function (attributes, record) {
                 currentRate = map_range(parseInt(element.split(",")[1]), 0, maxNumber, 0, 60);
             }
         });
-        
         if (currentRate === undefined) {
             return configuration;
         }
-
         currentRateColors = color_Map(currentRate);
-        
         //Color between  0 and 1
         configuration.attributes.interiorColor = new WorldWind.Color(
             currentRateColors[0],
@@ -52,7 +48,6 @@ var shapeConfigurationCallback = function (attributes, record) {
             currentRateColors[2],
             1.0
         );
-        
 
         // Paint the outline in a darker variant of the interior color. Fija
         configuration.attributes.outlineColor = new WorldWind.Color(
@@ -64,22 +59,23 @@ var shapeConfigurationCallback = function (attributes, record) {
 
     return configuration;
 };
+/* Function to add a layer with the different countrys */
+export function create_country_layer() {
+    //Countries layer
+    let shapefileLibrary = "https://worldwind.arc.nasa.gov/web/examples/data/shapefiles/naturalearth";
 
+    let worldLayer = new WorldWind.RenderableLayer("Countries");
+    let worldShapefile = new WorldWind.Shapefile(shapefileLibrary + "/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp");
+    worldShapefile.load(null, shapeConfigurationCallback, worldLayer);
+    wwd.addLayer(worldLayer);
+}
 
-//Countries layer
-let shapefileLibrary = "https://worldwind.arc.nasa.gov/web/examples/data/shapefiles/naturalearth";
-
-let worldLayer = new WorldWind.RenderableLayer("Countries");
-let worldShapefile = new WorldWind.Shapefile(shapefileLibrary + "/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp");
-worldShapefile.load(null, shapeConfigurationCallback, worldLayer);
-wwd.addLayer(worldLayer);
-
-
+/* Function to remap the numbers in a range to another range */
 function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
 
-
+/* Function for color map with number.*/
 function color_Map(number) {
     let purple = [0.19, 0.00, 0.40];
     let darkBlue = [0.00, 0.00, 0.80];
@@ -102,3 +98,5 @@ function color_Map(number) {
         return orange;
     }
 }
+
+create_country_layer();
